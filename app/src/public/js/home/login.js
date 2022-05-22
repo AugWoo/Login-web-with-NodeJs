95dbf945b7ee896c476d3fb950fffe320602a22c
AugWoo <103727168+AugWoo@users.noreply.github.com>
2022-05-22 18:31:05 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,7 +19,14 @@ function login() {
     body: JSON.stringify(req),
   })
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => {
+      if (json.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch(err => console.error(new Error('로그인 중 에러 발생')));
 }
 
 loginBtn.addEventListener('click', login);
